8b00888a8286a344fe1c6577c7abc38c37cee80c
Subashchandra <subhas9087@gmail.com>
2025-12-14 15:29:57 +0530

    I started doing new basic react project its the starting stage of it

---
+++ SIMPLE_REACT_PROJECT/Simple-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MovieCard from './components/MovieCard';
+
+function App(){
+  return(
+    <>
+    <MovieCard  movie={{title: "12th fail",release_date:"2024"}}/>
+
+    </>
+
+  );
+}
+
+export default App
